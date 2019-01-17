8a2259427635e60c3cc8627a7fa5dbcbc2c66136
Mahekaru <Mahekaru@yahoo.com>
2019-01-17 18:12:55 -0500

    converting my class into a more API style

---
+++ BinaryConverterAPI/Controllers/BinaryController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BinaryConverterAPI.Models;
+
+namespace BinaryConverterAPI.Controllers
+{
+    public class BinaryController : ApiController
+    {
+        public string GetData(int id, [FromUri] string value)
+        {
+            string s = "";
+
+            var b = new ConvertToBinary();
+
+            if (value != null)
+            {
+                if (id == 1)
+                {
+                    s = b.ToBinary(value);
+                }
+                else
+                {
+                    s = b.ToText(value);
+                }
+            }
+            else
+            {
+                Console.WriteLine("This is a friendly message...Your object is null");
+                //s = "This is a friendly message...Your object is null";
+            }
+            return s;
+        }
+    }
+}
---
+++ BinaryConverterAPI/Scripts/BinaryScript/Binary.js
@@ -0,0 +1,17 @@
+﻿var uri = 'api/Binary';
+
+function toBinary() {
+    var myText = $('#myText').val();
+    $.getJSON(uri + '/1?value=' + myText)
+        .done(function (data) {
+            $('#myBinary').val(data);
+        });
+}
+
+function toText() {
+    var myText = $('#myBinary').val();
+    $.getJSON(uri + '/2?value=' + myText)
+        .done(function (data) {
+            $('#myText').val(data);
+        });
+}
